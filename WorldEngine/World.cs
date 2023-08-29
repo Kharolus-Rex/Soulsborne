3468418742fb299668fed2556576b8314e5ce691
Connor Naylor <connor.r.naylor@gmail.com>
2023-08-29 15:13:55 -0500

    Created Classes folder under WorldEngine. Added weapons.csv to CSVFiles and filled in with data.

--- WorldEngine/World.cs
+++ WorldEngine/World.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace WorldEngine
 {
+    /* In this area will exist all of the lists
+     * for things such as Weapons, Rooms, Items
+     * Treasures, Monsters, etc.
+     * 
+     */
+
     internal class Program
     {
         static void Main(string[] args)
