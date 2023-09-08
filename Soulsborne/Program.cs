fcab7886f2cf74d2ab7b61fde9c72425b25f0fff
Connor Naylor <connor.r.naylor@gmail.com>
2023-09-07 19:24:54 -0500

    Added load statements to Program.cs for Soulsborne project.

--- Soulsborne/Program.cs
+++ Soulsborne/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WorldEngine;
 
 namespace Soulsborne
 {
@@ -10,6 +11,13 @@ namespace Soulsborne
     {
         static void Main(string[] args)
         {
+            //Load statements for lists
+            WorldLoad.LoadWeapons();
+            WorldLoad.LoadMonsters();
+            WorldLoad.LoadRooms();
+            WorldLoad.LoadPlayer();
+
+            //Gameplay loop to go below
         }
     }
 }
