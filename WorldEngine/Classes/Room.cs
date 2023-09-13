7722dcfe62e0c079a05406bf0a046c45bd4f16e2
da.ded <mcccarthD3455@student.faytechcc.edu>
2023-09-13 14:04:38 -0400

    Updated Rooms csv and class to have list consisting of  potions, weapons, monsters, treasures, and Items.

--- WorldEngine/Classes/Room.cs
+++ WorldEngine/Classes/Room.cs
@@ -17,8 +17,13 @@ namespace WorldEngine
         private int _exitWest;
         private int _exitUp;
         private int _exitDown;
+        private List<Potion> _potions;
+        private List<Weapon> _weapons;
+        private List<Monster> _monsters;
+        private List<Treasure> _treasures;
+        private List<Item> _items;
 
-        public Room(int idNumber, string name, string description, int exitNorth, int exitSouth, int exitEast, int exitWest, int exitUp, int exitDown)
+        public Room(int idNumber, string name, string description, int exitNorth, int exitSouth, int exitEast, int exitWest, int exitUp, int exitDown, List<Potion> potions, List<Weapon> weapons, List<Monster> monsters, List<Treasure> treasures, List<Item> items)
         {
             IdNumber = idNumber;
             Name = name;
@@ -29,6 +34,12 @@ namespace WorldEngine
             ExitWest = exitWest;
             ExitUp = exitUp;
             ExitDown = exitDown;
+            Potions = potions;
+            Weapons = weapons;
+            Monsters = monsters;
+            Treasures = treasures;
+            Items = items;
+
         }
         public int IdNumber { get { return _idNumber;} set { _idNumber = value; } }
         public string Name { get { return _name; } set { _name = value; } }
@@ -39,6 +50,11 @@ namespace WorldEngine
         public int ExitWest { get { return _exitWest; } set { _exitWest = value; } }
         public int ExitUp { get { return _exitUp; } set { _exitUp = value; } }
         public int ExitDown { get { return _exitDown; } set { _exitDown = value; } }
+        public List<Potion> Potions { get { return _potions; } set { _potions = value; } }
+        public List<Weapon> Weapons { get { return _weapons; } set { _weapons = value; } }
+        public List<Monster> Monsters { get { return _monsters; } set { _monsters = value; } }
+        public List<Treasure> Treasures { get { return _treasures; } set { _treasures = value; } }
+        public List<Item> Items { get { return _items; } set { _items = value; } }
 
     }
 }
