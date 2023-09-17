0b83278356486141b83ae5ce0d05908cfb5da670
Ramon Muniz Crespo <munizcrr0804@student.faytechcc.edu>
2023-09-17 16:46:15 -0400

    Updated potions.cs

--- WorldEngine/Classes/Potion.cs
+++ WorldEngine/Classes/Potion.cs
@@ -11,23 +11,27 @@ public class Potion
 {
     private int _idNumber;
     private string _name;
-    private int Action;
+    private string _description;
+    private int _value;
     
 
    
 
-    public Potion(int idNumber, string name, int action)
+    public Potion(int idNumber, string name, string description, int value)
     {
         IdNumber = idNumber;
 
         Name = name;
 
-        Action = action;
+        Description = description;
+
+        Value = value;
 
     }
     public int IdNumber { get { return _idNumber; } set { _idNumber = value; } }
     public string Name { get { return _name; } set { _name = value; } }
-    
+    public string Description { get { return _description; } set { _description = value; } }
+    public int Value { get { return _value; } set { _value = value; } }
     
     
 
