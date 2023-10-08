5102333c431ccc5531cf9708ddea8af7a99b321a
Connor Naylor <connor.r.naylor@gmail.com>
2023-10-08 14:58:40 -0500

    Adjusted combat to utilize AC for attacks.

--- WorldEngine/WorkingCombat.cs
+++ WorldEngine/WorkingCombat.cs
@@ -14,25 +14,48 @@ namespace WorldEngine
         {
             while (true)
             {
+                //general dice variable. used for both player and monster.
+                int diceRoll;
                 //player turn
-                int playerDamage = CalculateDamage(player.Weapons[0]);
-                monster.HP -= playerDamage;
-                Console.WriteLine($"{player.PlayerName} dealt {playerDamage} {player.Weapons[0].DamageType} damage to {monster.Name}.");
-                if ( monster.HP <= 0 )
+                int playerDamage;
+                //roll to hit
+                diceRoll = random.Next(1, 21);
+                if (diceRoll >= monster.AC)
                 {
-                    Console.WriteLine($"{monster.Name} is slain.");
-                    World.rooms[World.players[0].PlayerLocation].Monsters.RemoveAt(0);
-                    return;
+                    playerDamage = CalculateDamage(player.Weapons[0]);
+                    monster.HP -= playerDamage;
+                    Console.WriteLine($"{player.PlayerName} dealt {playerDamage} {player.Weapons[0].DamageType} damage to {monster.Name}.");
+                    if (monster.HP <= 0)
+                    {
+                        Console.WriteLine($"{monster.Name} is slain.");
+                        World.rooms[World.players[0].PlayerLocation].Monsters.RemoveAt(0);
+                        return;
+                    }
+                }
+                //if no hit
+                else
+                {
+                    Console.WriteLine($"{player.PlayerName} failed to hit {monster.Name}!");
                 }
 
                 //monster turn
-                int monsterDamage = CalculateDamage(monster.MonsterWeapon);
-                player.PlayerHP -= monsterDamage;
-                Console.WriteLine($"{monster.Name} deals {monsterDamage} {monster.MonsterWeapon.DamageType} damage to {player.PlayerName}.");
-                if ( player.PlayerHP <= 0 )
+                int monsterDamage;
+                //roll to hit
+                diceRoll = random.Next(1, 21);
+                if (diceRoll >= player.PlayerAC)
                 {
-                    Console.WriteLine($"{player.PlayerName} is slain.");
-                    ActiveGameplay.GameOver();
+                    monsterDamage = CalculateDamage(monster.MonsterWeapon);
+                    player.PlayerHP -= monsterDamage;
+                    Console.WriteLine($"{monster.Name} deals {monsterDamage} {monster.MonsterWeapon.DamageType} damage to {player.PlayerName}.");
+                    if (player.PlayerHP <= 0)
+                    {
+                        Console.WriteLine($"{player.PlayerName} is slain.");
+                        ActiveGameplay.GameOver();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"{monster.Name} failed to hit {player.PlayerName}!");
                 }
             }
         }
