ddeb5400c0d173ceec1f53297e4f211f22604eaa
Ramon Muniz Crespo <munizcrr0804@student.faytechcc.edu>
2023-10-08 22:05:43 -0400

    Created an interface

--- WorldEngine/Classes/Interface.cs
+++ WorldEngine/Classes/Interface.cs
@@ -3,48 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WorldEngine;
 
+namespace Interface
 {
-	interface class()
+
+ public interface Items
+{
+  int ID { get; set; }
+  string Name { get; set; }
+  string Description { get; set; }
+  bool Items { get; set; }
+}
+	public interface Monster
 	{
-		void Items();
-	    void Monster();
-	    void Player();
-	    void Potion();
-	    void Room();
-	    void Treasures();
-	    void weapon
-	    
+		int ID { get; set; }
+		string Name { get; set; }
+		string Description { get; set; }
+		string Race { get; set; }
+		int HP { get; set; }
+		int AC { get; set; }
+		bool Monster { get; set; }
+
+	public interface Player
+		{
+			int playerID { get; set; }
+			string playerName { get; set; }
+			string playerRace { get; set; }
+			int location { get; set; }
+			int playerHP { get; set; }
+			int playerAC { get; set; }
+			List<Weapon> _weapons { get; set; }
+			List<Items> _items { get; set; }
+			List<Potion> _potion { get; set; }
+			List<Treasures> _treasures { get; set; }
+			bool Player { get; set; }
+
+
+		}
+		public interface Potion { }
+
+		int idNumber { get; set; }
+		string name { get; set; }
+		string description { get; set; }
+		string value { get; set; }
+		bool potion { get; set; }
+
+		public interface Treasures
+		{
+			int IdNumber { get; set; }
+			string Name { get; set; }
+			string Value { get; set; }
+
+		}
+		
+
+		
+
+
+
+
 	}
-    
-    public static void items ()
-    {
-	  Console.WriteLine("Display the Items")
-    }
-    public static void Monster()
-    {
-    Console.WriteLine("Display the Monsters")
-    }
-    public static void Player()
-    {
-    Console.WriteLine("Display the Player")
-    }
-    public static void Potion()
-    {
-    Console.WriteLine("Display the Potions")
-    }
-    public static void Room()
-    {
-    Console.WriteLine("Display the Rooms")
-    }
-    public static void Treasures()
-    {
-    Console.WriteLine("Display the Treasures")
-    }
-    public static void weapon()
-    {
-    Console.WriteLine("Display the weapons")
-    }
+
 
 
 
@@ -52,4 +72,6 @@ using System.Threading.Tasks;
 
 }
 
+
+	
 
