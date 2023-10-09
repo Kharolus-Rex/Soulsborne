c308a9f08548cc19e6745268b2a65f6aa4f7b30c
Ramon Muniz Crespo <munizcrr0804@student.faytechcc.edu>
2023-10-08 20:20:41 -0400

    Created Interface

---
+++ WorldEngine/Classes/Interface.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+{
+	interface class()
+	{
+		void Items();
+	    void Monster();
+	    void Player();
+	    void Potion();
+	    void Room();
+	    void Treasures();
+	    void weapon
+	    
+	}
+    
+    public static void items ()
+    {
+	  Console.WriteLine("Display the Items")
+    }
+    public static void Monster()
+    {
+    Console.WriteLine("Display the Monsters")
+    }
+    public static void Player()
+    {
+    Console.WriteLine("Display the Player")
+    }
+    public static void Potion()
+    {
+    Console.WriteLine("Display the Potions")
+    }
+    public static void Room()
+    {
+    Console.WriteLine("Display the Rooms")
+    }
+    public static void Treasures()
+    {
+    Console.WriteLine("Display the Treasures")
+    }
+    public static void weapon()
+    {
+    Console.WriteLine("Display the weapons")
+    }
+
+
+
+
+
+}
+
+
