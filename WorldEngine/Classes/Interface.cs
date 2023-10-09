7ad44bbe3f6459c4e2a7c30a95edd26c7618b979
Ramon Muniz Crespo <munizcrr0804@student.faytechcc.edu>
2023-10-08 22:29:41 -0400

    Updated interface

--- WorldEngine/Classes/Interface.cs
+++ WorldEngine/Classes/Interface.cs
@@ -8,54 +8,15 @@ using WorldEngine;
 namespace Interface
 {
 
- public interface Items
+ public interface IGeninfo
 {
   int ID { get; set; }
   string Name { get; set; }
   string Description { get; set; }
-  bool Items { get; set; }
+  
 }
-	public interface Monster
-	{
-		int ID { get; set; }
-		string Name { get; set; }
-		string Description { get; set; }
-		string Race { get; set; }
-		int HP { get; set; }
-		int AC { get; set; }
-		bool Monster { get; set; }
-
-	public interface Player
-		{
-			int playerID { get; set; }
-			string playerName { get; set; }
-			string playerRace { get; set; }
-			int location { get; set; }
-			int playerHP { get; set; }
-			int playerAC { get; set; }
-			List<Weapon> _weapons { get; set; }
-			List<Items> _items { get; set; }
-			List<Potion> _potion { get; set; }
-			List<Treasures> _treasures { get; set; }
-			bool Player { get; set; }
-
-
-		}
-		public interface Potion { }
-
-		int idNumber { get; set; }
-		string name { get; set; }
-		string description { get; set; }
-		string value { get; set; }
-		bool potion { get; set; }
-
-		public interface Treasures
-		{
-			int IdNumber { get; set; }
-			string Name { get; set; }
-			string Value { get; set; }
-
-		}
+	
+}
 		
 
 		
@@ -63,14 +24,12 @@ namespace Interface
 
 
 
-	}
+	
 
 
 
 
 
-
-}
 
 
 	
